1a69b9e67ff24ef41c187b2657ed3fd5aed8708c
renanmaringolo <renanmaringolo@gmail.com>
2018-02-12 23:08:46 -0200

    initial commit

---
+++ exercise_1.rb
@@ -0,0 +1,36 @@
+######
+# 1) Imprima o nome completo sem modificar o valor da string original
+######
+
+nome = 'Renan'
+sobrenome = 'Proenca'
+
+# seu codigo aqui, use o metodo puts para imprimir o nome completo
+
+###### NAO MEXER #######
+puts(nome) # deve ser Renan, nao pode mudar!
+puts(sobrenome) # deve ser Proenca, nao pode mudar!
+
+######
+# 2) Imprima o nome completo MODIFICANDO o valor da string original
+######
+
+nome = 'Renan'
+sobrenome = ' Proenca'
+
+# seu codigo aqui, use o metodo puts para imprimir o nome completo
+
+###### NAO MEXER #######
+puts(nome) # deve ser Renan Proenca
+puts(sobrenome) # deve ser Proenca, nao pode mudar!
+
+######
+# 3) Crie um metodo que soma dois valores
+######
+
+# seu codigo aqui
+
+###### NAO MEXER #######
+puts soma(1, 2) # deve ser 3
+
+
