f276d14a2189ea3e7606459d7290f60d07c57e3e
renanmaringolo <renanmaringolo@gmail.com>
2018-02-14 12:23:19 -0200

    send exercise

--- session_2/exercise.rb
+++ session_2/exercise.rb
@@ -1,37 +1,29 @@
+def calcula_imposto(salario)
+ 
+   if salario < 5000
+    salario * 0.15
+   elsif salario >= 5000 && salario < 8000
+    salario * 0.20
+   else
+    salario * 0.30 
+   end
+
+
+end
+
 salarios = [1_000, 2_000, 5_000, 10_000]
 
 posicao = 0
 salario = salarios[posicao] # inicialmente sera 1_000
 
-#while salario != nil do
-  # chama o metodo "calcula_imposto" e atribui numa variavel "imposto"
-  # imprime (puts) "Salario: #{salario} - IR: #{imposto}"
-  # incrementa o valor 1 na posicao (lembra?)
-  # tem que mudar o salario pra nao dar loop infinito (olha pra linha 4)
-#end
-
-### Regras pro calculo do imposto
-# - ate 5000 (< 5000) => 15%
-# - de 5000 a 8000 (>= 5000 && < 8000) => 20%
-# - a partir de 8000 (> 8000) => 30%
-# Exemplos:
-# calcula_imposto(500) => 75.0
-# calcula_imposto(6000) => 1200.0
-###
-def calcula_imposto(salario)
-  # Opcao 1
-  # if (condicao)
-    # salario * aliquota
-  # elsif (outra condicao)
-    # salario * outra aliquota
-  # else
-    # salario * maior aliquota
-  # end
-
-  # Opcao 2
-  # return salario * 0.15 if salario < 5_000
-  # e por ai vai...
+while salario != nil do
+  imposto = calcula_imposto(salario) 
+  puts "Salario: #{salario} - IR #{imposto}"
+  posicao = posicao + 1
+  salario = salarios[posicao]
+  
 end
+
 
 ###### Resultado esperado #######
 
