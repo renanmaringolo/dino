3f5712c0ddf437d0056c51a44cc3914a7b51047d
renanmaringolo <renan.maringolo@gmail.com>
2018-02-14 18:30:46 -0200

    sendo exercise_3

---
+++ session_2/exercise_3.rb
@@ -0,0 +1,13 @@
+##dado um array com numeros, retornar a soma deles
+# Exemplo: 
+# numeros = [1, 2, 3] # 1 + 2 + 3 = 6
+# Saída no terminal (output): "A soma é 6"
+
+numeros = [1.+(2).+(3)]
+
+soma = 0 
+
+# use o for
+for soma in numeros
+puts "A soma é #{soma}"
+end
