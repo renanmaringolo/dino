39c59ff36c921b2606987a9678373aea217b6ee5
Leandro Maríngolo <leandronsp@gmail.com>
2018-02-14 14:54:26 +0000

    more exercise

---
+++ session_2/exercise_2.rb
@@ -0,0 +1,12 @@
+# Crie um array de nomes, com 5 elementos, e imprima uma mensagem de boas-vindas
+# pra cada nome.
+
+###### Resultado esperado #######
+
+# Seja bem-vindo, Leandro
+# Seja bem-vindo, Renan
+# Seja bem-vindo, Mara
+# Seja bem-vindo, Mike
+# Seja bem-vindo, Safira
+
+#################################
