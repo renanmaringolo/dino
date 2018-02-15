f33fa353f579b2a60f9dbbb021cca82f0ca11b46
renanmaringolo <renan.maringolo@gmail.com>
2018-02-15 00:40:18 -0200

    send exercise_3

--- session_2/exercise_3.rb
+++ session_2/exercise_3.rb
@@ -2,12 +2,24 @@
 # Exemplo: 
 # numeros = [1, 2, 3] # 1 + 2 + 3 = 6
 # Saída no terminal (output): "A soma é 6"
-
-numeros = [1.+(2).+(3)]
-
+#a) inicializa o array de numeros
+#b) inicializa a variavel soma (começa com zero)
+#c) pra cada numero no array
+#  c.1) adiciona o numero à variavel soma, o resultado disso atribui na variavel soma (sobrescreve)
+#d) imprime a variavel soma na tela interpolada com a mensagem
+  
+numeros = [1, 2, 3]
 soma = 0 
 
-# use o for
-for soma in numeros
-puts "A soma é #{soma}"
+
+
+for num in numeros
+  soma = soma + num 
+
 end
+
+
+puts "A soma é #{soma}"
+
+ 
+
