9abd62b1c1c57358baef866e8789cb6e59e45614
renanmaringolo <renan.maringolo@gmail.com>
2018-02-18 12:15:55 -0300

    send exercise.rb

--- session_3/exercise.rb
+++ session_3/exercise.rb
@@ -5,42 +5,22 @@
 # - O extrato mostra o saldo atual
 # - Sair do menu deve fechar o programa
 
-puts '====== BANCO DINOSSAURO ======'
-puts '## Menu ##'
-puts '#1 - Depósito'
-puts '#2 - Saque'
-puts '#3 - Extrato'
-puts '#0 - Sair'
-puts
-
-
-
-
-
+      
 
 #A partir daqui, vamos elaborar uma série de exercícios incrementais e numerados.
 # Os dois primeiros exercícios abaixo já estão feitos e servem
 # como base para os próximos exercícios.
 
+
 # 1 - Imprimir no screen a mensagem 'Escolha uma opção' (use o print ao inves do puts)
-print('Escolha uma opção: ')
-
-# 2 - Receber do gets a opção digitada e converter para inteiro, atribuindo
-# o resultado na variável opcao.
-opcao = gets.to_i
-
-# 3 - Inicializar a variável 'saldo' com 0. Todo saldo começa com zero.
-
-saldo = 0
-
-# 4 Colocar menu em loop, para fazer com que a opção "0" saia do programa
-
-while opcao != 0 do
-
-  # 5 - Mostrar o menu, com os prints/puts igual está lá em cima, e no final, atribuir à
-  # variável "opcao" o valor do gets (o gets tem que estar por ultimo dentro do while
 
 
+
+# 2 - Receber do gets a opção digitada e converter para inteiro, atribuind o resultado na variável opcao.
+
+
+
+def chama_menu
   puts '====== BANCO DINOSSAURO ======'
   puts '## Menu ##'
   puts '#1 - Depósito'
@@ -50,11 +30,34 @@ while opcao != 0 do
   puts
   
   print('Escolha uma opção: ')
-  opcao = gets.to_i  
+  gets.to_i
+end
+
+# 3 - Inicializar a variável 'saldo' com 0. Todo saldo começa com zero.
+
+saldo = 0
+opcao = chama_menu
+
+# 4 Colocar menu em loop, para fazer com que a opção "0" saia do programa
+
+while opcao != 0 do
+    
 
 
+
+
+
+  opcao = chama_menu
+  
+    
+  # 5 - Mostrar o menu, com os prints/puts igual está lá em cima, e no final, atribuir à
+  # variável "opcao" o valor do gets (o gets tem que estar por ultimo dentro do while
 
 end
+
+  # 6 criar um método para simplificar a chamada do menu para evitar codigo duiplicado (*codigo no topo*)
+
+
 
   puts "Até logo"
 
