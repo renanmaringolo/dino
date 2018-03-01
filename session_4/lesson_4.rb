161b7b4abe3108037d2c03aa836508a8503595fb
renanmaringolo <renan.maringolo@gmail.com>
2018-02-28 21:39:07 -0300

    send lesson_4.rb

--- session_4/lesson_4.rb
+++ session_4/lesson_4.rb
@@ -8,3 +8,30 @@
 # 1ABC999
 # Renan Proença
 # active
+
+saque = 400
+data = Time.now #tudo bem né? gostei desse método haha
+identificador = '1ABC999'
+pessoa = 'Renan Proença'
+status = 'active'
+
+movimento_saque = "#{saque}|#{data}|#{identificador}|#{pessoa}|#{status}"
+
+
+valores = movimento_saque.split('|')
+
+puts valores.size
+
+
+puts valores[0]
+puts valores[1]
+puts valores[2]
+puts valores[3]
+puts valores[4]
+puts valores[5] # pegadinha do malandro
+
+
+
+
+
+
