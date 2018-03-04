92eb51b4f45d96f7654dcdad6f12fd9aaf747022
renanmaringolo <renan.maringolo@gmail.com>
2018-03-04 00:33:25 -0300

    send lesson_8.rb

--- session_4/lesson_8.rb
+++ session_4/lesson_8.rb
@@ -15,10 +15,16 @@ def login
   print 'RG: '
   rg = gets.strip
 
+  print 'Cidade: '
+  cidade = gets.strip
+  
+  print 'estado_civil: '
+  estado_civil = gets.strip
+
   # MUDAR AQUI
   # receita dos dados da pessoa
   # propriedades: nome, email, cpf e rg
-  "#{nome}|#{email}|#{cpf}|#{rg}"
+  { nome: nome, email: email, cpf: cpf, rg: rg, cidade: cidade, estado_civil: estado_civil } 
 end
 
 def chama_menu
@@ -45,8 +51,21 @@ end
 # BOLO
 # MUDAR AQUI TBM
 # Renan|renan@gmail|12345|0999 VAI DEIXAR DE SER ASSIM!
-dados_da_pessoa = login # produziu um "bolo" da receita
 
+cadastros = []
+
+dados_da_pessoa_1 = login # produziu um "bolo" da receita
+dados_da_pessoa_2 = login
+dados_da_pessoa_3 = login
+dados_da_pessoa_4 = login
+
+cadastros << dados_da_pessoa_1
+cadastros << dados_da_pessoa_2
+cadastros << dados_da_pessoa_3
+cadastros << dados_da_pessoa_4
+
+identificador = '3BN98FDS'
+data = Time.now
 opcao = chama_menu
 movimentos = []
 saques = []
@@ -57,19 +76,19 @@ while opcao != 0 do
     print 'Valor do depósito: '
     deposito = gets.to_i
     depositos << deposito
-    movimentos << "Depósito realizado de: #{deposito}"
+    movimentos << "Depósito realizado de: #{deposito}, às #{data}, no terminal: #{identificador}"
   elsif opcao == 2
     saldo = calcula_saldo(depositos, saques)
     print 'Valor do saque: '
     saque = gets.to_i
-
+   
     if saldo < saque
       puts 'Valor insuficiente'
     else
       saques << saque
       # fique a vontade pra interpolar com as propriedades do "bolo"
       # ou nao, vc que sabe...
-      movimentos << "Saque realizado de: #{saque}"
+      movimentos << "Saque realizado de: #{saque}, às #{data}, no terminal: #{identificador}"
     end
   elsif opcao == 3
     saldo = calcula_saldo(depositos, saques)
@@ -81,8 +100,14 @@ while opcao != 0 do
 end
 
 # MUDAR A PARTIR DAQUI
-dados = dados_da_pessoa.split('|') # array
-nome = dados[0]
+cadastros.each do |dados| # array
+  nome = dados[:nome]
+  puts "Nome: #{nome}"
+  rg = dados[:rg]
+  puts "RG: #{rg}"
+
+  puts "Sessão encerrada às #{data}, obrigado, #{nome}, por confiar no Dino Bank S/A"
+
+end 
 
 # fique a vontade pra mudar aqui, colocar mais "propriedades" dos dados_da_pessoa
-puts "Obrigado #{nome} por contratar nossos serviços. Dinossauro Bank S/A"
