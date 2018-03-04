bfbed7a5f767cb2cbf360858dfbbe193dfd22cf9
Leandro Maríngolo <leandronsp@gmail.com>
2018-03-04 19:18:44 +0000

    correcao do exercicio

--- session_4/lesson_8.rb
+++ session_4/lesson_8.rb
@@ -17,14 +17,14 @@ def login
 
   print 'Cidade: '
   cidade = gets.strip
-  
+
   print 'estado_civil: '
   estado_civil = gets.strip
 
   # MUDAR AQUI
   # receita dos dados da pessoa
   # propriedades: nome, email, cpf e rg
-  { nome: nome, email: email, cpf: cpf, rg: rg, cidade: cidade, estado_civil: estado_civil } 
+  { nome: nome, email: email, cpf: cpf, rg: rg, cidade: cidade, estado_civil: estado_civil }
 end
 
 def chama_menu
@@ -52,20 +52,9 @@ end
 # MUDAR AQUI TBM
 # Renan|renan@gmail|12345|0999 VAI DEIXAR DE SER ASSIM!
 
-cadastros = []
-
-dados_da_pessoa_1 = login # produziu um "bolo" da receita
-dados_da_pessoa_2 = login
-dados_da_pessoa_3 = login
-dados_da_pessoa_4 = login
-
-cadastros << dados_da_pessoa_1
-cadastros << dados_da_pessoa_2
-cadastros << dados_da_pessoa_3
-cadastros << dados_da_pessoa_4
+dados_da_pessoa = login # produziu um "bolo" da receita
 
 identificador = '3BN98FDS'
-data = Time.now
 opcao = chama_menu
 movimentos = []
 saques = []
@@ -76,19 +65,19 @@ while opcao != 0 do
     print 'Valor do depósito: '
     deposito = gets.to_i
     depositos << deposito
-    movimentos << "Depósito realizado de: #{deposito}, às #{data}, no terminal: #{identificador}"
+    movimentos << "Depósito realizado de: #{deposito}, às #{Time.now}, no terminal: #{identificador}"
   elsif opcao == 2
     saldo = calcula_saldo(depositos, saques)
     print 'Valor do saque: '
     saque = gets.to_i
-   
+
     if saldo < saque
       puts 'Valor insuficiente'
     else
       saques << saque
       # fique a vontade pra interpolar com as propriedades do "bolo"
       # ou nao, vc que sabe...
-      movimentos << "Saque realizado de: #{saque}, às #{data}, no terminal: #{identificador}"
+      movimentos << "Saque realizado de: #{saque}, às #{Time.now}, no terminal: #{identificador}"
     end
   elsif opcao == 3
     saldo = calcula_saldo(depositos, saques)
@@ -100,14 +89,5 @@ while opcao != 0 do
 end
 
 # MUDAR A PARTIR DAQUI
-cadastros.each do |dados| # array
-  nome = dados[:nome]
-  puts "Nome: #{nome}"
-  rg = dados[:rg]
-  puts "RG: #{rg}"
-
-  puts "Sessão encerrada às #{data}, obrigado, #{nome}, por confiar no Dino Bank S/A"
-
-end 
-
-# fique a vontade pra mudar aqui, colocar mais "propriedades" dos dados_da_pessoa
+nome = dados_da_pessoa[:nome]
+puts "Sessão encerrada às #{Time.now}, obrigado, #{nome}, por confiar no Dino Bank S/A"
